f388bd5d527f08e2c1a5d380be09274570e58289
Sebastian Haas <dev@sebastianhaas.info>
2024-12-23 17:37:43 +0100

    Refined lualine a lil

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,23 +1,23 @@
 require('lualine').setup {
   options = {
-    icons_enabled = false,
+    icons_enabled = true,
     theme = 'auto',
     component_separators = { left = '', right = '|'},
     section_separators = { left = '', right = '|'},
     ignore_focus = {},
     always_divide_middle = true,
-    globalstatus = false,
+    globalstatus = true,
     refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
+      statusline = 100,
+      tabline = 100,
+      winbar = 100,
     }
   },
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch'},
     lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = {'encoding', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
