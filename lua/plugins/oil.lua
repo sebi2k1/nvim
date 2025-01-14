2af3a4a823ec1aac908b12db06c9d14241cd42f1
Sebastian Haas <dev@sebastianhaas.info>
2025-01-14 21:37:34 +0100

    Basic configuration for oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,4 +4,134 @@ return {
     ---@type oil.SetupOpts
     opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("oil").setup({
+            default_file_explorer = true,
+            columns = {
+                "icon",
+                "permissions",
+                "size",
+            },
+            -- Buffer-local options to use for oil buffers
+            buf_options = {
+                buflisted = false,
+                bufhidden = "hide",
+            },
+            -- Window-local options to use for oil buffers
+            win_options = {
+                wrap = false,
+                signcolumn = "no",
+                cursorcolumn = false,
+                foldcolumn = "0",
+                spell = false,
+                list = false,
+                conceallevel = 3,
+                concealcursor = "nvic",
+            },
+            delete_to_trash = false,
+            skip_confirm_for_simple_edits = false,
+            prompt_save_on_select_new_entry = true,
+            cleanup_delay_ms = 2000,
+            lsp_file_methods = {
+                enabled = true,
+                timeout_ms = 1000,
+                autosave_changes = false,
+            },
+            constrain_cursor = "editable",
+            watch_for_changes = false,
+            keymaps = {
+                ["<CR>"] = "actions.select",
+                ["<C-c>"] = { "actions.close", mode = "n" },
+                ["-"] = { "actions.parent", mode = "n" },
+                ["_"] = { "actions.open_cwd", mode = "n" },
+                ["gs"] = { "actions.change_sort", mode = "n" },
+            },
+            use_default_keymaps = true,
+            view_options = {
+                show_hidden = true,
+                is_hidden_file = function(name, bufnr)
+                    local m = name:match("^%.")
+                    return m ~= nil
+                end,
+                is_always_hidden = function(name, bufnr)
+                    return false
+                end,
+                natural_order = "fast",
+                case_insensitive = false,
+                sort = {
+                    { "type", "asc" },
+                    { "name", "asc" },
+                },
+                highlight_filename = function(entry, is_hidden, is_link_target, is_link_orphan)
+                    return nil
+                end,
+            },
+            extra_scp_args = {},
+            git = {
+                add = function(path)
+                    return false
+                end,
+                mv = function(src_path, dest_path)
+                    return false
+                end,
+                rm = function(path)
+                    return false
+                end,
+            },
+            -- Configuration for the floating window in oil.open_float
+            float = {
+                padding = 2,
+                max_width = 0,
+                max_height = 0,
+                border = "rounded",
+                win_options = {
+                    winblend = 0,
+                },
+                get_win_title = nil,
+                preview_split = "auto",
+                override = function(conf)
+                    return conf
+                end,
+            },
+            preview_win = {
+                update_on_cursor_moved = true,
+                preview_method = "fast_scratch",
+                disable_preview = function(filename)
+                    return false
+                end,
+                win_options = {},
+            },
+            confirmation = {
+                max_width = 0.9,
+                min_width = { 40, 0.4 },
+                width = nil,
+                max_height = 0.9,
+                min_height = { 5, 0.1 },
+                height = nil,
+                border = "rounded",
+                win_options = {
+                    winblend = 0,
+                },
+            },
+            progress = {
+                max_width = 0.9,
+                min_width = { 40, 0.4 },
+                width = nil,
+                max_height = { 10, 0.9 },
+                min_height = { 5, 0.1 },
+                height = nil,
+                border = "rounded",
+                minimized_border = "none",
+                win_options = {
+                    winblend = 0,
+                },
+            },
+            ssh = {
+                border = "rounded",
+            },
+            keymaps_help = {
+                border = "rounded",
+            },
+        })
+    end,
 }
